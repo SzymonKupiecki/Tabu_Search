3b7ed6b1b442d4e216b8cff1abeba145b370adc7
JanKwapien <81531382+JanKwapien@users.noreply.github.com>
2022-12-05 22:15:28 +0100

    Update Jan.py

--- Jan.py
+++ Jan.py
@@ -5,7 +5,7 @@ class matrix_to_solve:
     def __init__(self, hard_matrix: np.ndarray[np.ndarray[int]], cable_vector: np.ndarray[(float, float)], cost_tuple: tuple):
         self.num_of_buildings = len(hard_matrix) # liczba budynków
         self.cable_vector =  cable_vector # [(koszt kabla, przesył)]
-        self.cost_tuple = cost_tuple # (nr_budynku, żądany przesył, zysk, kara)
+        self.cost_tuple = cost_tuple # (nr_budynku, żądany przesył, zysk, kara),  stworzyć liste indeks = nr budynku
         self.hard_matrix = hard_matrix # macierz trudności
         self.connection_matrix = [[[None for i in range(3)] for j in range(len(hard_matrix))] for k in range(len(hard_matrix))]
 
