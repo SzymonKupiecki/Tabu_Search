07ff6307b0defdefba32807fcdb82a6418fdfb41
JakubPietrucha <81679184+JakubPietrucha@users.noreply.github.com>
2022-12-06 13:49:34 +0100

    Add files via upload

--- Jakub.py
+++ Jakub.py
@@ -2,29 +2,30 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 
-def Cost_function(difficulty_matrix,connection_matrix,available_wires):
-    total_cost = 0
-    cost = 0
-    for id_verse,connection in enumerate(connection_matrix):
-        for id_row,wires in enumerate(connection):
-            if wires != np.inf:
+def Cost_function(difficulty_matrix,connection_matrix,available_wires): #Funkcja obliczająca koszty budowy sieci
+    total_cost = 0 #Całkowity koszt za budowę sieci
+    cost = 0 #indeks mówiący o koszcie danego kabla
+    for id_verse,connection in enumerate(connection_matrix): #Iteracja po wszystkich połączeniach danego budynku
+        for id_row,wires in enumerate(connection): #Iteracja po wszystkich rodzajach kabla w danym połączeniu
+            if wires != np.inf: #Jeżeli połączenie istnieje to oblicz koszty jego utworzenia
                 for id_wire,amount_of_wire in enumerate(wires):
+                    #Dodaj do całkowitego kosztu koszt jednego połączenia
                     total_cost += available_wires[id_wire][cost] * amount_of_wire * difficulty_matrix[id_verse][id_row]
     return total_cost
 
-def Estimate_of_benefits_losses(vector_of_beneficts_losses, conection_matrix, vector_of_request,available_wires):
-    benefits = 0
-    losses = 1
-    transmition = 1
-    total_balance = 0
-    for dorm_id,conections in enumerate(conection_matrix):
-        total_transmit = 0
-        for neighbour in conections:
-            if neighbour != np.inf:
+def Estimate_of_benefits_losses(vector_of_beneficts_losses, conection_matrix, vector_of_request,available_wires): #Funkcja szacująca zyzki i straty generowane z sieci
+    benefits = 0 #Indeks zysków
+    losses = 1 #Indeks strat
+    transmition = 1 #Indeks przesyłu danego rodzaju kabla
+    total_balance = 0 #Całkowity bilans
+    for dorm_id,conections in enumerate(conection_matrix): #Iterowanie po każdym akademiku
+        total_transmit = 0 #Całkowity przesył dla danego akademika
+        for neighbour in conections: #Iteracja po połączeniach  akademikiem
+            if neighbour != np.inf: #Jeżeli połączenie istnieje to dodaj jego przesył do całkowitego przesyłu
                 for id_wire,amount_of_wire in enumerate(neighbour):
                     total_transmit += available_wires[id_wire][transmition] * amount_of_wire
-        if total_transmit >= vector_of_request[dorm_id]: total_balance += vector_of_beneficts_losses[dorm_id][benefits]
-        else: total_balance -= vector_of_beneficts_losses[dorm_id][losses]
+        if total_transmit >= vector_of_request[dorm_id]: total_balance += vector_of_beneficts_losses[dorm_id][benefits] #Dodaj zyski jeżeli spełniony wymagany przesył
+        else: total_balance -= vector_of_beneficts_losses[dorm_id][losses] #W przeciwnym przypadku odejmij od całkowitego bilansu straty
     return total_balance
 
 
