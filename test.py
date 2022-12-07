bc36a81829e7ad46dedf8aa3ad3768b9dbf7f825
SzymonKupiecki <kupieckiszymon@gmail.com>
2022-12-07 14:28:17 +0100

    poprawki

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 def dfs(adj_list):
     visited = list()
@@ -22,15 +23,17 @@ def transfer_list(max_conncetion_matrix, adj_lst, cost_tuple):
     usage_lst = [0 for i in range(len(adj_lst.keys()))]
     visiting_order = dfs(adj_lst)
     last_elem_indeks = len(visiting_order) - 1
-    usage_lst[0] = 10**10  # server has infinite
+    usage_lst[0] = np.inf  # server has infinite
     current_index = 1
     parent_index = 0
     while current_index <= last_elem_indeks:
         current_vertex = visiting_order[current_index]
         parent_vertex = visiting_order[parent_index]
         zadany_przesyl_rodzica, _, _ = cost_tuple[parent_vertex]
-        usage_lst[current_vertex] = min(max_conncetion_matrix[current_vertex][parent_vertex],
+        transfer = min(max_conncetion_matrix[current_vertex][parent_vertex],
                                         usage_lst[parent_vertex] - zadany_przesyl_rodzica)
+        usage_lst[current_vertex] += transfer
+        usage_lst[parent_vertex] -= transfer
 
         if usage_lst[current_vertex] < 0:
             usage_lst[current_vertex] = 0
@@ -39,12 +42,14 @@ def transfer_list(max_conncetion_matrix, adj_lst, cost_tuple):
         current_index = current_index + 1
         if current_index > last_elem_indeks:
             break
-
-        if current_index not in adj_lst[parent_index]:
+        current_vertex = visiting_order[current_index]
+        parent_vertex = visiting_order[parent_index]
+        while current_vertex not in adj_lst[visiting_order[parent_index]]:
             parent_index = parent_index - 1
-            # temp_parent_vertex
-            p, _, _ = cost_tuple[parent_vertex + 1]
-            usage_lst[parent_index] += max(usage_lst[parent_index + 1] - p, 0)
+            temp_prev_parent_vertex = visiting_order[parent_index + 1]
+            temp_act_parent_vertex = visiting_order[parent_index]
+            p, _, _ = cost_tuple[temp_prev_parent_vertex]
+            usage_lst[temp_act_parent_vertex] += max(usage_lst[parent_index + 1] - p, 0)
 
     return usage_lst
 
