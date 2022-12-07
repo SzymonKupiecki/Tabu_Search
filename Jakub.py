ca805a706571e578ae297802d8db02b9fd7dbc75
SzymonKupiecki <kupieckiszymon@gmail.com>
2022-12-07 14:44:41 +0100

    poprawa nazw funkcji

--- Jakub.py
+++ Jakub.py
@@ -4,7 +4,7 @@ import numpy as np
 from Jan import matrix_to_solve
 from Szymon import Solution
 
-def Cost_function(difficulty_matrix,connection_matrix,available_wires): #Funkcja obliczająca koszty budowy sieci
+def cost_function(difficulty_matrix,connection_matrix,available_wires): #Funkcja obliczająca koszty budowy sieci
     total_cost = 0 #Całkowity koszt za budowę sieci
     cost = 0 #indeks mówiący o koszcie danego kabla
     for id_verse,connection in enumerate(connection_matrix): #Iteracja po wszystkich połączeniach danego budynku
@@ -15,7 +15,7 @@ def Cost_function(difficulty_matrix,connection_matrix,available_wires): #Funkcja
                     total_cost += available_wires[id_wire][cost] * amount_of_wire * difficulty_matrix[id_verse][id_row]
     return total_cost
 
-def Estimate_of_benefits_losses(vector_of_beneficts_losses, conection_matrix, vector_of_request,available_wires): #Funkcja szacująca zyzki i straty generowane z sieci
+def estimate_of_benefits_losses(vector_of_beneficts_losses, conection_matrix, vector_of_request,available_wires): #Funkcja szacująca zyzki i straty generowane z sieci
     benefits = 0 #Indeks zysków
     losses = 1 #Indeks strat
     transmition = 1 #Indeks przesyłu danego rodzaju kabla
@@ -30,7 +30,7 @@ def Estimate_of_benefits_losses(vector_of_beneficts_losses, conection_matrix, ve
         else: total_balance -= vector_of_beneficts_losses[dorm_id][losses] #W przeciwnym przypadku odejmij od całkowitego bilansu straty
     return total_balance
 
-def Trasnsmission_allocation(solution: Solution,Matrix_to_solve: matrix_to_solve,vector_of_request):
+def trasnsmission_allocation(solution: Solution,Matrix_to_solve: matrix_to_solve,vector_of_request):
     stack = [0]
     visited = list()
     list_of_transmission = vector_of_request
@@ -45,7 +45,7 @@ def Trasnsmission_allocation(solution: Solution,Matrix_to_solve: matrix_to_solve
                         stack.append(u_idx)
                         visited.append(u)
 
-def Transmission_allocation_2(solution: Solution,Matrix_to_solve: matrix_to_solve,vector_of_request):
+def transmission_allocation_2(solution: Solution,Matrix_to_solve: matrix_to_solve,vector_of_request):
     def dfs(node,visited,Matrix_to_solve: matrix_to_solve,vector_of_request):
         if node not in visited:
             visited.append(node)
@@ -55,7 +55,7 @@ def Transmission_allocation_2(solution: Solution,Matrix_to_solve: matrix_to_solv
                     if result: vector_of_request[node] += result
             return vector_of_request[node]
 
-def Matrix_of_max_transfer(Matrix_to_solve:matrix_to_solve):
+def matrix_of_max_transfer(Matrix_to_solve:matrix_to_solve):
     matrix_of_max_transfer = np.zeros(Matrix_to_solve.connection_matrix)
     for id_dorm,dormitory in enumerate(Matrix_to_solve.connection_matrix):
         for id_connect,connection in enumerate(dormitory):
