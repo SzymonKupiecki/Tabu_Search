9b3f77e04762fda19dd27ffd9b3f13ab3963b167
SzymonKupiecki <kupieckiszymon@gmail.com>
2022-12-07 14:59:17 +0100

    nie wiem co xd

--- quality.py
+++ quality.py
@@ -1,4 +1,22 @@
 import numpy as np
+from Jan import matrix_to_solve
+from Szymon import Solution
+
+
+def matrix_of_max_transfer(Matrix_to_solve:matrix_to_solve):
+    matrix_of_max_transfer = np.zeros(Matrix_to_solve.connection_matrix)
+    for id_dorm,dormitory in enumerate(Matrix_to_solve.connection_matrix):
+        for id_connect,connection in enumerate(dormitory):
+            max_transfer = 0
+            if connection is not None and connection != np.inf:
+                for id_wire,wire in enumerate(connection):
+                    max_transfer += Matrix_to_solve.cable_vector[id_wire][1]
+                matrix_of_max_transfer[id_dorm][id_connect] = max_transfer
+    for i in range(1, len(matrix_of_max_transfer)):
+        for j in range(i):
+            matrix_of_max_transfer[j][i] = matrix_of_max_transfer[i][j]
+    return matrix_of_max_transfer
+
 
 def dfs(adj_list):
     visited = list()
@@ -53,4 +71,33 @@ def transfer_list(max_conncetion_matrix, adj_lst, cost_tuple):
 
     return usage_lst
 
+def estimate_of_benefits_losses(vector_of_beneficts_losses, conection_matrix, vector_of_request,available_wires): #Funkcja szacująca zyzki i straty generowane z sieci
+    benefits = 0 #Indeks zysków
+    losses = 1 #Indeks strat
+    transmition = 1 #Indeks przesyłu danego rodzaju kabla
+    total_balance = 0 #Całkowity bilans
+    for dorm_id,conections in enumerate(conection_matrix): #Iterowanie po każdym akademiku
+        total_transmit = 0 #Całkowity przesył dla danego akademika
+        for neighbour in conections: #Iteracja po połączeniach  akademikiem
+            if neighbour != np.inf: #Jeżeli połączenie istnieje to dodaj jego przesył do całkowitego przesyłu
+                for id_wire,amount_of_wire in enumerate(neighbour):
+                    total_transmit += available_wires[id_wire][transmition] * amount_of_wire
+        if total_transmit >= vector_of_request[dorm_id]: total_balance += vector_of_beneficts_losses[dorm_id][benefits] #Dodaj zyski jeżeli spełniony wymagany przesył
+        else: total_balance -= vector_of_beneficts_losses[dorm_id][losses] #W przeciwnym przypadku odejmij od całkowitego bilansu straty
+    return total_balance
 
+def cost_function(difficulty_matrix,connection_matrix,available_wires): #Funkcja obliczająca koszty budowy sieci
+    total_cost = 0 #Całkowity koszt za budowę sieci
+    cost = 0 #indeks mówiący o koszcie danego kabla
+    for id_verse,connection in enumerate(connection_matrix): #Iteracja po wszystkich połączeniach danego budynku
+        for id_row,wires in enumerate(connection): #Iteracja po wszystkich rodzajach kabla w danym połączeniu
+            if wires != np.inf: #Jeżeli połączenie istnieje to oblicz koszty jego utworzenia
+                for id_wire,amount_of_wire in enumerate(wires):
+                    #Dodaj do całkowitego kosztu koszt jednego połączenia
+                    total_cost += available_wires[id_wire][cost] * amount_of_wire * difficulty_matrix[id_verse][id_row]
+    return total_cost
+
+
+def quality():
+    # polaczyc wszytskie powyzsze
+    pass
