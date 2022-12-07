82d1a747a3e580124809b3f4d0a3ce1a282e0c98
JakubPietrucha <81679184+JakubPietrucha@users.noreply.github.com>
2022-12-07 10:19:57 +0100

    Add files via upload

--- Jakub.py
+++ Jakub.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import numpy as np
+from Jan import matrix_to_solve
+from Szymon import Solution
 
 def Cost_function(difficulty_matrix,connection_matrix,available_wires): #Funkcja obliczająca koszty budowy sieci
     total_cost = 0 #Całkowity koszt za budowę sieci
@@ -27,5 +29,21 @@ def Estimate_of_benefits_losses(vector_of_beneficts_losses, conection_matrix, ve
         if total_transmit >= vector_of_request[dorm_id]: total_balance += vector_of_beneficts_losses[dorm_id][benefits] #Dodaj zyski jeżeli spełniony wymagany przesył
         else: total_balance -= vector_of_beneficts_losses[dorm_id][losses] #W przeciwnym przypadku odejmij od całkowitego bilansu straty
     return total_balance
+
+def Trasnsmission_allocation(solution: Solution,Matrix_to_solve: matrix_to_solve,vector_of_request):
+    stack = [0]
+    visited = list()
+    list_of_transmission = vector_of_request
+    while stack:
+        v = stack.pop()
+        if v not in visited:
+            visited.append(v)
+            for u_idx,u in enumerate(Matrix_to_solve.connection_matrix[v]):
+                if u is not None and u != np.inf:
+                    if u not in visited:
+                        list_of_transmission[v] += list_of_transmission[u_idx]
+                        stack.append(u_idx)
+                        visited.append(u)
+
 
 
