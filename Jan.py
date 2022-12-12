79f8748533a063b0e51e58b8f6c7193349440488
SzymonKupiecki <kupieckiszymon@gmail.com>
2022-12-12 19:39:35 +0100

    wyrzucenie niepotrzebnych rzeczy

--- Jan.py
+++ Jan.py
@@ -7,21 +7,11 @@ class matrix_to_solve: # zmien nazwe
     def __init__(self, hard_matrix, cable_vector, cost_tuple):
         self.num_of_buildings = len(hard_matrix)  # liczba budynków
         self.cable_vector = cable_vector  # [(koszt kabla, przesył)]
-        self.cost_tuple = cost_tuple  # (nr_budynku, żądany przesył, zysk, kara),  stworzyć liste indeks = nr budynku
+        self.cost_tuple = cost_tuple  # (żądany przesył, zysk, kara), indeks = nr budynku
         self.hard_matrix = hard_matrix  # macierz trudności
-        # self.connection_matrix = [[[None for i in range(3)] for j in range(len(hard_matrix))] for k in range(len(hard_matrix))]
 
-    def show_matrix(self, matrix):
-        for row in matrix:
-            for val in row:
-                print(val, end=" ")
-            print()
-
-    # def insert_connection(self, num_row, num_col):
-    #     if num_row != num_col:
-    #         if num_row > num_col:
-    #             num_row, num_col = num_col, num_row
-    #         for i in range(3):
-    #             self.connection_matrix[num_row][num_col][i] = 0
-    #     else:
-    #         print("Nie można łączyć tych samych budynków")
+    # def show_matrix(self, matrix):
+    #     for row in matrix:
+    #         for val in row:
+    #             print(val, end=" ")
+    #         print()
