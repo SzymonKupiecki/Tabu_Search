1287b5cc787e15bc7b0e26b135a080873311e665
SzymonKupiecki <kupieckiszymon@gmail.com>
2022-12-12 20:51:24 +0100

    wywołanie testowe

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
-from quality import transfer_list
+import numpy as np
+from Jan import matrix_to_solve
+from Szymon import Solution, sample_matrix_generator, is_valid, optimize
 
-max_connection = [[0, 90, 0, 0, 40, 0],
-                  [90, 0, 30, 30, 0, 0],
-                  [0, 30, 0, 0, 0, 10],
-                  [0, 30, 0, 0, 0, 0],
-                  [40, 0, 0, 0, 0, 0],
-                  [0, 0, 10, 0, 0]]
+# sample data
+hard_matrix = [[0, 20, 10, 15, 80, 60],
+               [20,  0, 10, 40, 15, 10],
+               [10, 10,  0,  5, 30, 20],
+               [15, 40,  5,  0, 40, 25],
+               [80, 15, 30, 40,  0, 5],
+               [60, 10, 20,  25,  5, 0]]
 
-lst_tup = [(0, 0, 0), (40, 15, 15), (20, 15, 15), (80, 15, 15), (20, 15, 15), (20, 15, 15)]
+cable_vector = [(15, 10), (25, 20), (50, 40)]
 
-matrix = {
-    0: [1, 4],
-    1: [0, 2, 3],
-    2: [1, 5],
-    3: [1],
-    4: [0],
-    5: [2]
-}
+cost_tuples = [(0, 0, 0), (90, 300, 15), (110, 450, 70), (80, 150, 40), (70, 250, 20), (120, 100, 90)]
 
-hope = transfer_list(max_connection, matrix, lst_tup)
-print(hope)
+info = matrix_to_solve(np.array(hard_matrix), np.array(cable_vector), np.array(cost_tuples))
+
+starting_solution = sample_matrix_generator(0, 6, 3, 0.35)
+
+valid_matrix = is_valid(starting_solution)
+i = 1
+
+while not valid_matrix and i < 10000:
+    starting_solution = sample_matrix_generator(i, 6, 3, 0.35)
+    i += 1
+    valid_matrix = is_valid(starting_solution)
+
+starting_solution = Solution(starting_solution, info)
+
+print(i)
+print(starting_solution)
+print(starting_solution.quality_)
+
+res = optimize(starting_solution, info)
+print(res.quality_)
+print(res)
