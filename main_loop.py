e9772693da0cb3beca766366143bbc22a570e6e0
SzymonKupiecki <kupieckiszymon@gmail.com>
2022-12-15 19:00:54 +0100

    przystosowanie do ruchów wracających, dodanie kryterium aspiracji

--- main_loop.py
+++ main_loop.py
@@ -1,32 +1,33 @@
 from dtypes import ProblemInfo
-from solution import Solution, find_neighbour
+from solution import Solution, find_neighbour_transfer, find_neighbour_connection, forbidden_moves
 from tabu_list import TabuList
 
 
-def optimize(starting_solution: Solution, info: ProblemInfo, tabu_length=10, iterations=200, raport=True):
+def optimize(starting_solution: Solution, info: ProblemInfo, tabu_length=10, iterations=200, raport=True,
+             number_of_neighbours_in_iteration=20):
     if raport:
         print(f"Start optymalizacji zadania:\n{starting_solution}\nz parametrami: długość tabu = {tabu_length}"
               f", ilość iteracji = {iterations}")
-    best_solution = starting_solution
-    last_solution = starting_solution
-    tabu = TabuList(tabu_length)  # INICJALIZACJA TABU
-    tabu.insert_elem(starting_solution)
+    best_solution = starting_solution  # variable to store best solution
+    last_solution = starting_solution  # variable to store solution that was taken in last iteration
+    tabu = TabuList(tabu_length)  # initialization of tabu list
     for i in range(iterations):
-        neighbours = []  # WYWOŁANIE FUNKCJI SZUKAJĄCEJ SĄSIADÓW
-        for _ in range(10):
-            neighbours.append(Solution(find_neighbour(last_solution, info), info))
-        neighbours.sort(key=lambda x: x.quality_, reverse=True)
+        neighbours = []  # list to store neighbours found in iteration
+        for _ in range(number_of_neighbours_in_iteration//2):  # finding neighbours, 50% of each type
+            neighbours.append(Solution(find_neighbour_transfer(last_solution, info), info))
+            neighbours.append(Solution(find_neighbour_connection(last_solution, info), info))
+        neighbours.sort(key=lambda x: x.quality_, reverse=True)  # sort by quality
         last_solution = None
         for candidate in neighbours:
-            if candidate in tabu:
+            #  criterion of aspiration - if found solution is better than best take it despite it's forbidden by tabu
+            if candidate in tabu and candidate.quality_ < best_solution.quality_:
                 continue
-            last_solution = candidate
-            # DODANIE LAST CANDIDATE DO LISTY TABU
-            tabu.insert_elem(last_solution)
+            last_solution = candidate  # take solution that fulfills requirements
+            tabu.insert_elem(forbidden_moves(last_solution))  # forbid all changes that will return to previous solution
             break
         if last_solution is None:
             print(f"Nie znaleziono kandydatów i =  {i}")
-            continue  # oznacza to, że nie umiemy znaleźć nowego sąsiada
+            continue  # we don't find correct neighbour, so we try another time
         if last_solution.quality_ > best_solution.quality_:
             best_solution = last_solution
             if raport:
