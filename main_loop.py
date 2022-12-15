117bfefd27ea36bd2d768f7f46555d891323142d
SzymonKupiecki <kupieckiszymon@gmail.com>
2022-12-15 22:24:16 +0100

    kompatybilny z find_neighbour_transfer

--- main_loop.py
+++ main_loop.py
@@ -9,25 +9,26 @@ def optimize(starting_solution: Solution, info: ProblemInfo, tabu_length=10, ite
         print(f"Start optymalizacji zadania:\n{starting_solution}\nz parametrami: długość tabu = {tabu_length}"
               f", ilość iteracji = {iterations}")
     best_solution = starting_solution  # variable to store best solution
-    last_solution = starting_solution  # variable to store solution that was taken in last iteration
+    last_solution = starting_solution  # variable to store solution that was taken in last correct iteration
     tabu = TabuList(tabu_length)  # initialization of tabu list
     for i in range(iterations):
         neighbours = []  # list to store neighbours found in iteration
         for _ in range(number_of_neighbours_in_iteration//2):  # finding neighbours, 50% of each type
-            neighbours.append(Solution(find_neighbour_transfer(last_solution, info), info))
-            neighbours.append(Solution(find_neighbour_connection(last_solution, info), info))
+            neighbours.append(find_neighbour_transfer(last_solution, info))
+            # neighbours.append(find_neighbour_connection(last_solution, info))
         neighbours.sort(key=lambda x: x.quality_, reverse=True)  # sort by quality
-        last_solution = None
+        next_solution = None
         for candidate in neighbours:
             #  criterion of aspiration - if found solution is better than best take it despite it's forbidden by tabu
             if candidate in tabu and candidate.quality_ < best_solution.quality_:
                 continue
-            last_solution = candidate  # take solution that fulfills requirements
+            next_solution = candidate  # take solution that fulfills requirements
             tabu.insert_elem(forbidden_moves(last_solution))  # forbid all changes that will return to previous solution
             break
-        if last_solution is None:
+        if next_solution is None:
             print(f"Nie znaleziono kandydatów i =  {i}")
-            continue  # we don't find correct neighbour, so we try another time
+            continue  # we don't find correct neighbour, so we try again
+        last_solution = next_solution  # if we found new solution assign it to proper variable
         if last_solution.quality_ > best_solution.quality_:
             best_solution = last_solution
             if raport:
